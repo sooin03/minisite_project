c1f785f5676221fd86144f8403201e6201ae208c
JEONG <sooin03@naver.com>
2021-11-01 15:56:19 +0900

    minisite 초기화 설정

---
+++ src/main/java/com/example/minisite_project/MinisiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.minisite_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinisiteProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinisiteProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minisite_project/MinisiteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minisite_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinisiteProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
